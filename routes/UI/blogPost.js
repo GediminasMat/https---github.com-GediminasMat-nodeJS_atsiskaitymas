10d3e53de3723f112b901649caabd7c2e8eeaf8b
GediminasMAt <matijosaitisgediminas@gmail.com>
2022-05-24 23:39:42 +0300

    Blogo postinimo aprašas

---
+++ routes/UI/blogPost.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import con from '../sql_connection.js';
+import isLoggedIn from '../isLoggedIn.js';
+
+const blogPost = express.Router();
+
+blogPost.post('/', async (req, res) => {
+  try {
+    if (await isLoggedIn(req)) {
+      const rb = req.body;
+      if (!rb.title || !rb.content) return res.send({ msg: 'Need title or content' });
+      await con.query(`INSERT INTO blog (title, content, author_id, created_at)VALUES (?,?,?,?) `,
+        [rb.title, rb.content, req.token.id, new Date().toLocaleString('LT')]);
+      res.redirect('/');
+    } else {return res.send({ msg: 'Neprisijungęs, negalima postint' });
+  }} catch (err) {res.status(500).send({ err: err.message });
+  }
+});
+
+blogPost.get('/', async (req, res) => {
+  const auth = await isLoggedIn(req);
+  res.render('blogPost', { page: 'blogPost', css: 'blogPost.css', auth: auth });
+});
+
+export default blogPost;
